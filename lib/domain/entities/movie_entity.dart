dedbc91ff3c0d95bbd1b1235311f22a895bd76ec
rohit-0308 <rohitkumar860311@gmail.com>
2021-04-01 17:57:32 +0530

    Movie_Entity & Movie_Model Completed

---
+++ lib/domain/entities/movie_entity.dart
@@ -0,0 +1,26 @@
+import 'package:equatable/equatable.dart';
+
+class MovieEntity extends Equatable {
+  final String posterPath;
+  final int id;
+  final String backdropPath;
+  final String title;
+  final num voteAverage;
+  final String releaseDate;
+  final String overview;
+
+  MovieEntity(
+      {this.posterPath,
+      this.id,
+      this.backdropPath,
+      this.title,
+      this.voteAverage,
+      this.releaseDate,
+      this.overview});
+
+  @override
+  List<Object> get props => [id, title];
+
+  @override
+  bool get stringify => true;
+}
