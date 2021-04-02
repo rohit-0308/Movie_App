a19d42d1d31f410cf69b96aebb122fa85873fc9e
rohit-0308 <rohitkumar860311@gmail.com>
2021-04-02 22:00:50 +0530

    Implemented dependencyInjection using GetIt Package

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:movies/data/core/api_client.dart';
-import 'package:movies/data/data_sources/movie_remote_data_sources.dart';
-import 'package:movies/data/repositories/movie_repository_impl.dart';
 import 'package:movies/domain/entities/app_error.dart';
 import 'package:movies/domain/entities/movie_entity.dart';
 import 'package:movies/domain/entities/no_params.dart';
-import 'package:movies/domain/repositories/movie_repositories.dart';
 import 'package:movies/domain/usecases/get_trending.dart';
+import 'package:pedantic/pedantic.dart';
+import 'dependency_injections(di)/get_it.dart' as getIt;
 
 void main() async {
-  ApiClient apiClient = ApiClient(Client());
-  MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(apiClient);
-  MovieRepository movieRepository = MovieRepositoryImpl(dataSource);
-  GetTrending getTrending = GetTrending(movieRepository);
+  unawaited(getIt.init());
+  GetTrending getTrending = getIt.getItInstance<GetTrending>();
   final Either<AppError, List<MovieEntity>> eitherRespone =
       await getTrending(NoParams());
   eitherRespone.fold((l) {
@@ -32,11 +27,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: Container(
+        color: Colors.red,
+      ),
     );
   }
 }
